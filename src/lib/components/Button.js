20c374e555026f02118d5133dfabdc4432afbd90
aman <aman.dhamanda@infoedge.com>
2024-11-09 12:19:02 +0530

    changes done for the npm package

---
+++ src/lib/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export const Button = ({btnText}) => {
+  return(
+    <button onClick={() => {console.log("It worked!")}}>
+      {btnText}
+    </button>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/lib/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'MyButton',
+    libraryTarget: 'umd',
+    publicPath: '/dist'
+  },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
+  },
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
+
+module.exports = config;
